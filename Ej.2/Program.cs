86c7a8b95931cdea66a031d84a8834b16e5ce987
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 15:23:03 +0200

    Numero max. Warning en parse

---
+++ Ej.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+Console.Write("Inserte el primer numero: ");
+        int a = int.Parse(Console.ReadLine());
+Console.Write("Inserte el segundo numero: ");
+        int b  = int.Parse(Console.ReadLine());
+Console.Write("Inserte el tercer numero: ");
+        int c = int.Parse(Console.ReadLine());
+    int max = a;
+    if (b>max)
+    {
+      max =  b;
+    }
+    if (c>max)
+    {
+        max=c;
+    }    
+Console.Write("El numero mayor es: ");
+Console.Write(max);
