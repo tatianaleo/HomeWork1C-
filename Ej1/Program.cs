7e9b15efe9e38b8dc03e9e35b9af38d94a227dd8
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 14:42:29 +0200

    Wrote a programe

--- Ej1/Program.cs
+++ Ej1/Program.cs
@@ -1,5 +1,22 @@
 ﻿//Напишите программу, которая на вход принимает два числа 
 //и выдаёт, какое число большее, а какое меньшее.
 
+int a =  new Random().Next(1,100);
+Console.WriteLine("El primer numero es: " );
+Console.WriteLine(a);
+int b =  new Random().Next(1,100);
+Console.WriteLine("El segundo numero es: " );
+Console.WriteLine(b);
+if (a<b)
+{
+  Console.WriteLine("El numero mayor es : ");
+  Console.WriteLine(b);  
+}
+else
+{
+  Console.WriteLine("El numero mayor es : "); 
+  Console.WriteLine(a);   
+}
 
-Console.WriteLine("Hello, World!");
+
+
