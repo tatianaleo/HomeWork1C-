458b4f36d1073db59966041e6f3820d6fca57a22
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 16:57:15 +0200

    Algoritmo, codigo y reamde

---
+++ Ej.4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+double n=13;
+int index = 2;
+if (n<=1)
+{
+  Console.Write("No hay numeros par positivos desde 1 hasta ");  
+  Console.WriteLine(n);
+}
+while (index<=n)
+{
+Console.WriteLine(index);
+index= index+2;
+}
