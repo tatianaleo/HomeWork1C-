f3afe2fe3731eb9d15771aa31f03f209cd5f1821
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 15:31:35 +0200

    Numero par

---
+++ Ej.3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Inserte el numero: ");
+int a = int.Parse(Console.ReadLine());
+if (a%2==0)
+{
+  Console.WriteLine("El numero es par");  
+}
+else{
+    Console.WriteLine("El numero es impar");
+}
